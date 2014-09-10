6e3e188cd6477aa9a65f3830dcf8e6a64afd7df2
amisukanta <amisukanta02@gmail.com>
2014-09-10 12:03:42 +0200

    migration changed

--- db/migrate/20140904092647_add_company_idto_boxelements.rb
+++
@@ -1,9 +0,0 @@
-class AddCompanyIdtoBoxelements < ActiveRecord::Migration
-  def up
-  	add_column :boxelements, :company_id, :integer
-  end
-
-  def down
-  	remove_column :boxelements, :company_id
-  end
-end
