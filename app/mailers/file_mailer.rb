10c317e92bd0d896b08fef915f9628e1d6f432b2
amisukanta <amisukanta02@gmail.com>
2015-04-09 13:14:52 +0200

    File link in email

--- app/mailers/file_mailer.rb
+++ app/mailers/file_mailer.rb
@@ -4,22 +4,15 @@ class FileMailer < ActionMailer::Base
   def send_email_file_user(fileusers, file, id)
     @file = file
     @file_id = id
-    puts "File data"
-    puts @file_id
     filedata = LinkedFile.where(boxelement_id: id).first
     @filename = filedata.filename
-    puts @filename
     fileusers.each do |m|
       @user_file = User.find(m)
       @user = @user_file.name
      
-     #puts "File deliver email"
-     #puts @user_file.mail
-     #puts @user
-      mail to: @user_file.mail, subject: " A new file #{@filename} is added to Knowledge Management"
+     
+      mail to: @user_file.mail, subject: " A new file #{@filename} is added to Knowledge Management. "
     end
   end
-  def send_email_permitted_users
-
-  end
+  
 end
